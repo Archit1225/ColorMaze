dbec5e94cbdf7cf7f39599a4b88450f1281ff359
Archit1225 <rakgupta1978@gmail.com>
2026-07-18 13:18:47 +0530

    Small fixes/tweaks

--- Assets/Scripts/CameraShakeManager.cs
+++ Assets/Scripts/CameraShakeManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CameraShakeManager : MonoBehaviour
 {
     public static CameraShakeManager Instance;
-    [SerializeField] private float globalShakeForce = 1f;
+    [SerializeField] private float globalShakeForce = 1.5f;
     private void Awake()
     {
         if (Instance == null) Instance = this;
--- Assets/Scripts/ChangeScenes.cs
+++ Assets/Scripts/ChangeScenes.cs
@@ -3,8 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+    [SerializeField] private GameObject quitButton;
+
     public void ChangeScene(int sceneNo)
     {
         SceneManager.LoadScene(sceneNo);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Attempting to exit the game...");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // Exits standalone builds
+            Application.Quit();
+#endif
+    }
+
+    private void Start()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            // Disable the quit button so WebGL players never even see it
+            if (quitButton != null)
+            {
+                quitButton.SetActive(false);
+            }
+#endif
     }
 }
